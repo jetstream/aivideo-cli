c365bdcb9f8dc290119e65388fc104e613776836
Roy Sprowl <roys@jetstreamsoftware.com>
2019-06-14 17:11:55 -0700

    Change to new versions 3.0.0

--- solutions/remotemonitoring/single-vm/scripts/update.sh
+++ solutions/remotemonitoring/single-vm/scripts/update.sh
@@ -4,7 +4,7 @@ PCS_RELEASE_VERSION=$1
 
 if [ -z "$PCS_RELEASE_VERSION" ]; then
     echo "Please specify release version for which you want to run the update"
-    echo "For latest release number, please see https://github.com/Azure/azure-iot-pcs-remote-monitoring-dotnet/releases"
+    echo "For latest release number, please see https://github.com/Azure-Samples/AI-Video-Intelligence-Solution-Accelerator/releases"
     exit 1
 fi
 
